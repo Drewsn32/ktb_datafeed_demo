97fc646b2fa318da0a21c9c131172645d325124d
drewsn32 <drewsn32@gmail.com>
2021-01-27 16:41:55 +0700

    init

---
+++ WebApplication1/Controllers/KTBController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [AllowAnonymous]
+    [Route("v1/[controller]")]
+    [ApiController]
+    public class KTBController : Controller
+    {
+        [HttpGet]
+        [Route("DataFeed")]
+        public IActionResult GetDataFeed([FromBody]object model)
+        {
+            return Ok("OK");
+        }
+    }
+}
